3c6121b708e638de281575731717977ee2389d43
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-04-26 22:59:18 +0200

    Customer Model

--- core/models.py
+++ core/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=False, blank=False
+    )
+    phone_field = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.user.username
