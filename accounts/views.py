d299ea2ed787d572207cf006efea53666a44088d
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-04-27 02:40:37 +0200

    Messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from core.models import Customer
 
 
@@ -12,7 +13,7 @@ def user_login(request):
         if user is not None:
             login(request, user)
             return redirect('index')
-        
+        messages.info(request, "LogIn Failed please try again")
     return render(request, 'accounts/login.html')
 
 
@@ -26,33 +27,34 @@ def user_register(request):
 
         if password == confirm_password:
             if User.objects.filter(username=username).exists():
-                print("USerName is Already exist .. ")
-                return redirect('user_register')            
+                messages.info(request, "The username Is already taken")
+                return redirect('user_register')
 
             else:
                 if User.objects.filter(email=email).exists():
-                    print("Email is Already exist .. ")
+                    messages.info(request, "The Email Is already taken")
                     return redirect('user_register')
                 else:
                     user = User.objects.create_user(
                         username=username, email=email, password=password
-                        )
+                    )
                     user.save()
-                    
+
                     customer_data = Customer(
                         user=user, phone_field=phone_field
-                        )
+                    )
                     customer_data.save()
 
                     # Code for logging
                     our_user = authenticate(
                         username=username, password=password
-                        )
+                    )
                     if our_user is not None:
                         login(request, user)
                         return redirect('index')
         else:
-            return redirect('user_register')            
+            messages.info(request, "The PassWord does not match")
+            return redirect('user_register')
 
     return render(request, 'accounts/register.html')
 
